7ef20347622a79561e0c1ec40d680bcf0e7a061b
Jake Gordon <jake@codeincomplete.com>
2011-05-12 21:17:34 -0700

    implemented Object.create and tidy up Game and Game.Runner objects

--- reference/game.js
+++ reference/game.js
@@ -1,29 +1,37 @@
 //=============================================================================
-// Function.bind
 //
-// part of ECMAScript 5 and is provided native in some modern browsers, but
-// in older browsers we need to implement it ourselves:
-//  (see https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind)
+// We need some ECMAScript 5 methods but we need to implement them ourselves
+// for older browsers (compatibility: http://kangax.github.com/es5-compat-table/)
+//
+//  Function.bind:        https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind
+//  Object.create:        http://javascript.crockford.com/prototypal.html
+//  Object.extend:        (defacto standard like jquery $.extend or prototype's Object.extend)
 //
 //=============================================================================
   
 if (!Function.prototype.bind) {
-  Function.prototype.bind = function(){
-    var _$A = function(a){return Array.prototype.slice.call(a);}
-    if(arguments.length < 2 && (typeof arguments[0] == "undefined")) return this;
-    var __method = this, args = _$A(arguments), object = args.shift();
-    return function() {
-      return __method.apply(object, args.concat(_$A(arguments)));
-    }
+  Function.prototype.bind = function(obj) {
+    var slice = [].slice,
+        args  = slice.call(arguments, 1),
+        self  = this,
+        nop   = function () {},
+        bound = function () {
+          return self.apply(this instanceof nop ? this : (obj || {}), args.concat(slice.call(arguments)));   
+        };
+    nop.prototype   = self.prototype;
+    bound.prototype = new nop();
+    return bound;
   };
 }
 
-//=============================================================================
-// Object.extend - shallow merge of source properties into target
-//
-// Provided by many 3rd party libraries (e.g. jquery $.extend or prototype Object.extend)
-// but we might have to provide it ourself if no libraries are being used for this game.
-//=============================================================================
+if (!Object.create) {
+  Object.create = function(base) {
+    function F() {};
+    F.prototype = base;
+    return new F();
+  }
+}
+
 if (!Object.extend) {
   Object.extend = function(destination, source) {
     for (var property in source)
@@ -36,20 +44,21 @@ if (!Object.extend) {
 // GAME
 //=============================================================================
 
-Game = function(id, game, config) { /* constructor */
-  this.init(id, game, config);
-};
-
-//-----------------------------------------------------------------------------
-
-Object.extend(Game, { /* class methods */
+Game = { /* static methods */
 
   compatible: function() {
-    return Game.ua.hasCanvas;
+    return Object.create &&
+           Object.extend &&
+           Function.bind &&
+           Game.ua.hasCanvas
   },
 
   start: function(id, game, config) {
-    return Game.compatible() ? new Game(id, game, config) : null;
+    if (Game.compatible()) {
+      var g = Object.create(Game.Runner);
+      g.start(id, game, config);
+      return g;
+    }
   },
 
   ua: function() {
@@ -158,116 +167,117 @@ Object.extend(Game, { /* class methods */
     P:        80,
     Q:        81,
     TILDA:    192
-  }
-
-});
-
-//-----------------------------------------------------------------------------
-
-Game.prototype = { /* instance methods */
-
-  init: function(id, game, config) {
-    this.fps          = 60;
-    this.interval     = 1000.0 / this.fps;
-    this.canvas       = document.getElementById(id);
-    this.front        = this.canvas;
-    this.back         = Game.createCanvas();
-    this.back.width   = this.front.width;
-    this.back.height  = this.front.height;
-    this.front2d      = this.front.getContext('2d');
-    this.back2d       = this.back.getContext('2d');
-    this.debug        = config.debug || (location.href.indexOf("grdebug") > 0);
-    this.showStats    = this.debug;
-
-    Game.loadImages(game.Images, this.initGame.bind(this, game, config));
   },
 
-  initGame: function(game, customConfig, images) {
-    var config = {};                                           // build up config object to pass to game constructor...
-    Object.extend(config, game.Defaults ? game.Defaults : {}); // start off with game defaults (if any)
-    Object.extend(config, customConfig  ? customConfig  : {}); // extend with customized config (if any)
-    config.width  = this.canvas.width;                         // add width
-    config.height = this.canvas.height;                        // add height
-    config.images = images;                                    // add images
-    this.game = new game(this, config);                        // ... and finally construct the game object
-    Game.addEvent(document, 'keydown', this.onkeydown.bind(this));
-    Game.addEvent(document, 'keyup',   this.onkeyup.bind(this));
-    this.lastFrame = Game.timestamp();
-    this.resetStats();
-    setInterval(this.loop.bind(this), this.interval);
-  },
+  //-----------------------------------------------------------------------------
 
-  loop: function() {
-    var start  = Game.timestamp(); this.update((start - this.lastFrame)/1000.0); // send dt as seconds
-    var middle = Game.timestamp(); this.draw();
-    var end    = Game.timestamp();
-    this.updateStats(middle - start, end - middle);
-    this.lastFrame = start;
-  },
+  Runner: { /* instance methods */
 
-  update: function(dt) {
-    this.game.update(dt);
-  },
+    start: function(id, game, config) {
+      this.fps          = 60;
+      this.interval     = 1000.0 / this.fps;
+      this.canvas       = document.getElementById(id);
+      this.front        = this.canvas;
+      this.back         = Game.createCanvas();
+      this.back.width   = this.front.width;
+      this.back.height  = this.front.height;
+      this.front2d      = this.front.getContext('2d');
+      this.back2d       = this.back.getContext('2d');
+      this.debug        = config.debug || (location.href.indexOf("debug") > 0);
+      this.showStats    = this.debug;
 
-  draw: function() {
-    this.back2d.clearRect(0, 0, this.back.width, this.back.height);
-    this.game.draw(this.back2d);
-    this.drawStats(this.back2d);
-    this.front2d.clearRect(0, 0, this.front.width, this.front.width);
-    this.front2d.drawImage(this.back, 0, 0);
-  },
+      Game.loadImages(game.Images, this.initGame.bind(this, game, config));
+    },
 
-  resetStats: function() {
-    if (this.showStats) {
-      this.stats = {
-        count: 0, fps: 0,
-        update: { total: 0, average: 0 },
-        draw:   { total: 0, average: 0 },
-        frame:  { total: 0, average: 0 }  // update + draw
-      };
+    initGame: function(game, customConfig, images) {
+      var config = {};                                           // build up config object to pass to game constructor...
+      Object.extend(config, game.Defaults ? game.Defaults : {}); // start off with game defaults (if any)
+      Object.extend(config, customConfig  ? customConfig  : {}); // extend with customized config (if any)
+      config.width  = this.canvas.width;                         // add width
+      config.height = this.canvas.height;                        // add height
+      config.images = images;                                    // add images
+      this.game = new game(this, config);                        // ... and finally construct the game object
+      Game.addEvent(document, 'keydown', this.onkeydown.bind(this));
+      Game.addEvent(document, 'keyup',   this.onkeyup.bind(this));
+      this.lastFrame = Game.timestamp();
+      this.resetStats();
+      setInterval(this.loop.bind(this), this.interval);
+    },
+
+    loop: function() {
+      var start  = Game.timestamp(); this.update((start - this.lastFrame)/1000.0); // send dt as seconds
+      var middle = Game.timestamp(); this.draw();
+      var end    = Game.timestamp();
+      this.updateStats(middle - start, end - middle);
+      this.lastFrame = start;
+    },
+
+    update: function(dt) {
+      this.game.update(dt);
+    },
+
+    draw: function() {
+      this.back2d.clearRect(0, 0, this.back.width, this.back.height);
+      this.game.draw(this.back2d);
+      this.drawStats(this.back2d);
+      this.front2d.clearRect(0, 0, this.front.width, this.front.width);
+      this.front2d.drawImage(this.back, 0, 0);
+    },
+
+    resetStats: function() {
+      if (this.showStats) {
+        this.stats = {
+          count: 0, fps: 0,
+          update: { total: 0, average: 0 },
+          draw:   { total: 0, average: 0 },
+          frame:  { total: 0, average: 0 }  // update + draw
+        };
+      }
+    },
+
+    updateStats: function(update, draw) {
+      if (this.showStats) {
+        if (this.stats.count >= this.fps) // only keep averages for 1 second
+          this.stats.update.total = this.stats.draw.total = this.stats.frame.total = this.stats.count = 0;
+        this.stats.update.total  += update;
+        this.stats.draw.total    += draw;
+        this.stats.frame.total   += update + draw;
+        this.stats.count         += 1;
+        this.stats.update.average = Math.round(this.stats.update.total / this.stats.count);
+        this.stats.draw.average   = Math.round(this.stats.draw.total   / this.stats.count);
+        this.stats.frame.average  = Math.round(this.stats.frame.total  / this.stats.count);
+        this.stats.fps            = Math.min(this.fps, Math.round(1000/this.stats.frame.average));
+      }
+    },
+
+    drawStats: function(ctx) {
+      if (this.showStats) {
+        ctx.fillText("frame: "  + this.stats.count,                 this.back.width - 100, this.back.height - 50);
+        ctx.fillText("fps: "    + this.stats.fps,                   this.back.width - 100, this.back.height - 40);
+        ctx.fillText("update: " + this.stats.update.average + "ms", this.back.width - 100, this.back.height - 30);
+        ctx.fillText("draw: "   + this.stats.draw.average   + "ms", this.back.width - 100, this.back.height - 20);
+      }
+    },
+
+    onkeydown: function(ev) { if (this.game.onkeydown) this.game.onkeydown(ev.keyCode); },
+    onkeyup:   function(ev) { if (this.game.onkeyup)   this.game.onkeyup(ev.keyCode);   },
+
+    hideCursor: function() { this.canvas.style.cursor = 'none'; },
+    showCursor: function() { this.canvas.style.cursor = 'auto'; },
+
+    alert: function(msg) {
+      result = window.alert(msg);        // window.alert blocks the thread...
+      this.lastFrame = Game.timestamp(); // so we need to avoid sending huge dt values in the next update()
+      return result;
+    },
+
+    confirm: function(msg) {
+      result = window.confirm(msg);      // window.confirm blocks the thread...
+      this.lastFrame = Game.timestamp(); // so we need to avoid sending huge dt values in the next update()
+      return result;
     }
-  },
 
-  updateStats: function(update, draw) {
-    if (this.showStats) {
-      if (this.stats.count >= this.fps) // only keep averages for 1 second
-        this.stats.update.total = this.stats.draw.total = this.stats.frame.total = this.stats.count = 0;
-      this.stats.update.total  += update;
-      this.stats.draw.total    += draw;
-      this.stats.frame.total   += update + draw;
-      this.stats.count         += 1;
-      this.stats.update.average = Math.round(this.stats.update.total / this.stats.count);
-      this.stats.draw.average   = Math.round(this.stats.draw.total   / this.stats.count);
-      this.stats.frame.average  = Math.round(this.stats.frame.total  / this.stats.count);
-      this.stats.fps            = Math.min(this.fps, Math.round(1000/this.stats.frame.average));
-    }
-  },
+    //-------------------------------------------------------------------------
 
-  drawStats: function(ctx) {
-    if (this.showStats) {
-      ctx.fillText("frame: "  + this.stats.count,                 this.back.width - 100, this.back.height - 50);
-      ctx.fillText("fps: "    + this.stats.fps,                   this.back.width - 100, this.back.height - 40);
-      ctx.fillText("update: " + this.stats.update.average + "ms", this.back.width - 100, this.back.height - 30);
-      ctx.fillText("draw: "   + this.stats.draw.average   + "ms", this.back.width - 100, this.back.height - 20);
-    }
-  },
-
-  onkeydown: function(ev) { if (this.game.onkeydown) this.game.onkeydown(ev.keyCode); },
-  onkeyup:   function(ev) { if (this.game.onkeyup)   this.game.onkeyup(ev.keyCode);   },
-
-  hideCursor: function() { this.canvas.style.cursor = 'none'; },
-  showCursor: function() { this.canvas.style.cursor = 'auto'; },
-
-  alert: function(msg) {
-    result = window.alert(msg);              // window.alert blocks the thread...
-    this.lastFrame = Game.timestamp(); // so we need to avoid sending huge dt values in the next update()
-    return result;
-  },
-
-  confirm: function(msg) {
-    result = window.confirm(msg);            // window.confirm blocks the thread...
-    this.lastFrame = Game.timestamp(); // so we need to avoid sending huge dt values in the next update()
-    return result;
-  }
-
-};
+  } // Game.Runner
+} // Game
