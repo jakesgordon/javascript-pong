91a4b973abf5bb393d0125af0b94954351f120c6
Jake Gordon <jake@codeincomplete.com>
2011-05-14 13:20:40 -0700

    added description to part3

--- part3/pong.js
+++ part3/pong.js
@@ -75,7 +75,7 @@ Pong = {
 
   goal: function(playerNo) {
     this.scores[playerNo] += 1;
-    if (this.scores[playerNo] == 9) {
+    if (this.scores[playerNo] == 1) {
       this.menu.declareWinner(playerNo);
       this.stop();
     }
