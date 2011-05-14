6b0fb471c75e619726403aad7f6a71120a4e4fa8
Jake Gordon <jake@codeincomplete.com>
2011-05-14 10:59:06 -0700

    added part1

---
+++ part1/pong.js
@@ -0,0 +1,43 @@
+//=============================================================================
+// PONG
+//=============================================================================
+
+Pong = {
+
+  Defaults: {
+    width:  640,   // logical canvas width (browser will scale to physical canvas size - which is controlled by @media css queries)
+    height: 480,   // logical canvas height (ditto)
+    stats: true,   // tell Game.Runner to show stats
+  },
+
+  //-----------------------------------------------------------------------------
+
+  initialize: function(runner, cfg) {
+    this.cfg         = cfg;
+    this.runner      = runner;
+    this.width       = runner.width;
+    this.height      = runner.height;
+    this.time        = 0;
+    this.runner.start();
+  },
+
+  update: function(dt) {
+    this.time = this.time + dt;
+  },
+
+  draw: function(ctx) {
+    ctx.strokeStyle = 'white'
+    ctx.strokeRect(0, 0, this.width, this.height);
+
+    ctx.fillStyle = '#1080F0'
+    ctx.font = '144px sans-serif'
+
+    var count = Math.round(this.time).toString();
+    var dim   = ctx.measureText(count); dim.height = dim.height || 100; // measureText only returns width!
+    var x     = (this.width - dim.width) / 2;
+    var y     = (this.height - dim.height) / 2;
+
+    ctx.fillText(count, x, y + dim.height);
+  }
+
+}; // Pong
