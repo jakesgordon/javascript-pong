09303a18a26169df93ba8e4c31ebad39303663a8
Jake Gordon <jake@codeincomplete.com>
2011-05-14 18:05:52 -0700

    fixed Object.extend to use hasOwnProperty correctly

--- part3/game.js
+++ part3/game.js
@@ -46,8 +46,10 @@ if (!Object.construct) {
 
 if (!Object.extend) {
   Object.extend = function(destination, source) {
-    for (var property in source)
-      destination[property] = source[property];
+    for (var property in source) {
+      if (source.hasOwnProperty(property))
+        destination[property] = source[property];
+    }
     return destination;
   };
 }
